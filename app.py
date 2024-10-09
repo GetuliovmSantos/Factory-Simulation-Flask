e023dc67bdfdc2f6f035a039ba6c86f7a1398038
GetuliovmSantos <getulio.vm.santos@gmail.com>
2024-10-08 21:21:24 -0300

    login
    implementado conexão com banco de dados e verificação de usuario

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request
+from bd import login
 
 app = Flask(__name__)
 
 @app.route('/')
-def login():
-    return render_template('login.html')
+def index():
+    return render_template('login.html', erro=False)
+
+@app.route('/logar', methods=['POST'])
+def logar():
+    usuario = request.form['id']
+    senha = request.form['senha']
+    print(usuario, senha)
+    resultado = login(usuario, senha)
+
+    if "error" in resultado:
+        return render_template('login.html', erro=True)
+    else:
+        return redirect('/fabrica')
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
--- bd.py
+++ bd.py
@@ -1,1 +1,43 @@
 #conecção banco de dados
+import mysql.connector
+# erro de conexão
+import mysql.connector.errors
+
+def criarConexao():
+    print("Conectando ao banco de dados...")
+    try:
+        conexao = mysql.connector.connect(
+            host="localhost",
+            port="3307",
+            user="jason",
+            password="derf120@",
+            database="fabrica"
+            )
+        print("Conexão realizada com sucesso!")
+        return conexao
+    except mysql.connector.errors.InterfaceError as erro:
+        print("Erro de conexão: ", erro)
+        return None
+
+def login(usuario, senha):
+    conexao = criarConexao()
+    if conexao == None:
+        return "Erro de conexão"
+    else:
+        cursor = conexao.cursor()
+        cursor.execute("select * from usuarios where idUsuarios = %s and senha = %s", (usuario, senha))
+        resultado = cursor.fetchall()
+        if len(resultado) > 0:
+            return {
+                "idUsuarios": resultado[0][0],
+                "nome": resultado[0][1],
+                "funcao": resultado[0][2],
+                "senha": resultado[0][3]
+            }
+        else:
+            return {
+                "error": True
+            }
+
+
+    
